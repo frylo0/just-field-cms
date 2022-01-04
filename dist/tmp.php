ed7bb9609c0259b412d40a00f88fe618452a7ea7
frity <nikonovfedor36936@gmail.com>
2022-01-04 14:20:11 +0300

    tmp.php: sendmail

--- dist/tmp.php
+++ dist/tmp.php
@@ -1,24 +1,45 @@
 <?php require '../php/jf.php'; ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>TMP</title>
-</head>
-<body style="background: grey">
-   <?php
-      function render() { ?>
-         <h1>Render</h1>
-      <?php }
-   ?>
+<html>
+   
+   <head>
+      <title>Sending HTML email using PHP</title>
+   </head>
+   
+   <body>
+      
+      <?php
+         if (array_key_exists('to', $_POST)) {
+            $to = $_POST['to'];
+            $subject = $_POST['subject'];
+            
+            $message = $_POST['message'];
+            
+            $header = "From:{$_POST['from']} \r\n";
+            $header .= "Cc:afgh@somedomain.com \r\n";
+            $header .= "MIME-Version: 1.0\r\n";
+            $header .= "Content-type: text/html\r\n";
+            
+            $retval = mail ($to,$subject,$message,$header);
+            
+            if( $retval == true ) {
+               echo "<strong style=\"color: green;\">Message sent successfully!!!</strong>";
+            }else {
+               echo "<strong style=\"color: red\">Message could not be sent...</strong>";
+            }
+         }
+      ?>
 
-   <?php if (pv('is_docs_visible')) : ?>
-      <h1>Docs</h1>
-      <?= pv('docs')->html; ?>
-   <?php endif; ?>
-   <?php render(); ?>
-</body>
+      <form action="./tmp.php" method="post">
+
+         <input type="text" name="from" id="" placeholder="from"><br>
+         <input type="text" name="to" id="" placeholder="to"><br>
+         <input type="text" name="subject" id="" placeholder="subject"><br>
+         <input type="text" name="message" id="" placeholder="message"><br>
+         
+         <input type="submit" value="send">
+
+      </form>
+      
+   </body>
 </html>
