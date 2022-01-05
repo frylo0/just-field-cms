3e63481842260c4c302911fb9f63c05e9cb541c3
frity <nikonovfedor36936@gmail.com>
2022-01-05 17:05:42 +0300

    table multiple ordering

--- src/Basic/table/table.js
+++ src/Basic/table/table.js
@@ -3,18 +3,23 @@ $(document).ready(() => {
    const updateLink = document.querySelector('table').dataset.updateLink;
    const parentId = document.querySelector('table').dataset.parentId;
    
-   $(`${pref}__order`).on('pointerdown', action('pointerdown: table order'));
+   $(`${pref}__order`).on('pointerdown', action(`pointerdown: ${pref} order`));
 
    window.isTableOrdering = false;
    window.tableTrsRect = [];
-   when('pointerdown: table order', e => {
-      /** @type {HTMLElement} */
-      const tr = e.currentTarget.parentElement;
-      const trRect = tr.getBoundingClientRect();
-      
-      tr.classList.add('table__tr_ordering');
+   when(`pointerdown: ${pref} order`, e => {
+      let trs = [e.currentTarget.parentElement];
 
-      const trsRect = $(`${pref} tbody tr`).toArray().filter(el => el != tr).map(tr => {
+      const $selection = $(`${pref} .tr_selected`);
+      if ($selection.length > 0) {
+         trs = $selection.toArray();
+      }
+      
+      for (let tr of trs) {
+         tr.classList.add(`${pref}__tr_ordering`.slice(1));
+      }
+
+      const trsRect = $(`${pref} tbody tr`).toArray().filter(el => !trs.includes(el)).map(tr => {
          const rect = tr.getBoundingClientRect();
          rect.middle = {
             x: (rect.left + rect.right) / 2,
@@ -22,61 +27,58 @@ $(document).ready(() => {
          };
          return {tr, rect};
       });
+
       window.tableTrsRect = trsRect;
-      window.isTableOrdering = [tr];
+      window.isTableOrdering = trs;
    });
    
    when('pointer: window', async (etype, e) => {
       if (isTableOrdering) {
-         for (let tr of window.isTableOrdering) {
+         if (etype == 'up') {
+            const $tr = $(window.isTableOrdering);
 
-            if (etype == 'up') {
-               const $tr = $(tr);
+            const $placeBefore = $(`${pref}__tr_possible-place-before`);
+            const $placeAfter = $(`${pref}__tr_possible-place-after`);
 
-               const $placeBefore = $(`${pref}__tr_possible-place-before`);
-               const $placeAfter = $(`${pref}__tr_possible-place-after`);
-
-               if ($placeBefore.length > 0) {
-                  $tr.insertBefore($placeBefore);
-                  $placeBefore.removeClass(`${pref}__tr_possible-place-before`.slice(1));
-               }
-               else if ($placeAfter.length > 0) {
-                  $tr.insertAfter($placeAfter);
-                  $placeAfter.removeClass(`${pref}__tr_possible-place-after`.slice(1));
-               }
-               else {
-                  console.warn('table.js: after tr move no possible place found.', tr);
-               }
-
-               tr.classList.remove('table__tr_ordering');
-               window.isTableOrdering = false;
-
-               const order = $(`${pref} tbody tr`).toArray().map(el => el.dataset.itemId).join(',');
-               console.log(order);
-               
-               let res = await fetchJsonOk('Ordering', updateLink, {
-                  method: 'POST',
-                  body: {
-                     item_id: parentId,
-                     colname: 'value',
-                     value: order,
-                  },
-               });
-            } 
-
-            else if (etype == 'move') {
-               $('.table__tr_possible-place-before').removeClass('table__tr_possible-place-before');
-               $('.table__tr_possible-place-after').removeClass('table__tr_possible-place-after');
-               
-               for (let {tr: el, rect} of window.tableTrsRect) {
-                  if (e.clientY < rect.middle.y) {
-                     el.classList.add('table__tr_possible-place-before');
-                     return;
-                  }
-               }
-               window.tableTrsRect[window.tableTrsRect.length - 1].tr.classList.add('table__tr_possible-place-after');
+            if ($placeBefore.length > 0) {
+               $tr.insertBefore($placeBefore);
+               $placeBefore.removeClass(`${pref}__tr_possible-place-before`.slice(1));
+            }
+            else if ($placeAfter.length > 0) {
+               $tr.insertAfter($placeAfter);
+               $placeAfter.removeClass(`${pref}__tr_possible-place-after`.slice(1));
+            }
+            else {
+               console.warn('table.js: after tr move no possible place found.', tr);
             }
 
+            $tr.removeClass(`${pref}__tr_ordering`.slice(1));
+            window.isTableOrdering = false;
+
+            const order = $(`${pref} tbody tr`).toArray().map(el => el.dataset.itemId).join(',');
+            console.log('order:', order);
+            
+            let res = await fetchJsonOk('Ordering', updateLink, {
+               method: 'POST',
+               body: {
+                  item_id: parentId,
+                  colname: 'value',
+                  value: order,
+               },
+            });
+         } 
+
+         else if (etype == 'move') {
+            $(`${pref}__tr_possible-place-before`).removeClass(`${pref}__tr_possible-place-before`.slice(1));
+            $(`${pref}__tr_possible-place-after`).removeClass(`${pref}__tr_possible-place-after`.slice(1));
+            
+            for (let {tr: el, rect} of window.tableTrsRect) {
+               if (e.clientY < rect.middle.y) {
+                  el.classList.add(`${pref}__tr_possible-place-before`.slice(1));
+                  return;
+               }
+            }
+            window.tableTrsRect[window.tableTrsRect.length - 1].tr.classList.add(`${pref}__tr_possible-place-after`.slice(1));
          }
       }
    })
