23678acea69fac24b0155db720a75777f0318d01
frity <nikonovfedor36936@gmail.com>
2022-01-16 23:26:00 +0300

    T_excel front

---
+++ src/Blocks/item_T_excel/item_T_excel.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+   const pref = '.item_T_excel'; // prefix for current folder
+   const updateLink = document.querySelector('table').dataset.updateLink;
+
+   when('rowHandle: excel', tr => {
+      const row = tr, item = tr;
+      const $buttonUpload = $(`${pref}__upload-button`, row);
+      const $buttonDownload = $(`${pref}__download-button`, row);
+      const $file = $(`${pref}__file`, row);
+
+      $buttonUpload.on('click', () => action(`click: ${pref} button upload`)($file));
+      $file.on('change', () => action(`change: ${pref} file`)($buttonDownload, $file, item));
+   });
+
+   when(`click: ${pref} button upload`, $file => {
+      $file.trigger('click');
+   });
+   when(`change: ${pref} file`, async ($buttonDownload, $file, item) => {
+      const fetchUrl = updateLink;
+
+      const formData = new FormData();
+      formData.append('item_id', $(item).find('[colname="value"]')[0].dataset.itemId);
+      formData.append('colname', 'value');
+      formData.append('value', $file[0].files[0]);
+
+      let res = await fetchJsonOk('Uploading', fetchUrl, {
+         method: 'POST',
+         body: formData,
+      });
+      
+      $buttonDownload.find('a').attr('href', res.data); // updating link on download button
+      $buttonDownload.removeAttr('disabled');
+   });
+});
